42850d20ff8dfd54713da32f0bd4e4e554d8b4e2
Sofiia Chorna <chernaya.sofiia@gmail.com>
2025-01-07 17:28:54 +0100

    Add pca to evaluation

--- src/eval.py
+++ src/eval.py
@@ -3,6 +3,8 @@ from datetime import datetime
 import torch
 import torchvision
 import wandb
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 from utils import load_checkpoint, get_device
 
 
@@ -13,18 +15,13 @@ def save_generated_samples(generator, output_dir, num_samples):
     labels = torch.randint(0, generator.n_classes, (num_samples,)).to(device)
     generated_samples = generator(noise, labels)
 
-    # generate names for files
-    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    file_name = f"generated_samples_{timestamp}.png"
-    labels_file_name = f"labels_{timestamp}.txt"
-
     # save generated images
     grid = torchvision.utils.make_grid(generated_samples.cpu(), nrow=8)
-    save_path = os.path.join(output_dir, file_name)
+    save_path = os.path.join(output_dir, "generated_samples.png")
     torchvision.utils.save_image(grid, save_path)
 
     # save labels
-    labels_save_path = os.path.join(output_dir, labels_file_name)
+    labels_save_path = os.path.join(output_dir, "labels.txt")
     with open(labels_save_path, 'w') as f:
         for label in labels.cpu().numpy():
             f.write(f"{label} ")
@@ -38,8 +35,26 @@ def save_generated_samples(generator, output_dir, num_samples):
     return generated_samples, labels
 
 
-def get_pca():
-    pass
+def get_pca(samples, labels, output_dir):
+    """Apply PCA on the samples"""
+    # flatten to (num_samples, num_features)
+    samples = samples.view(samples.size(0), -1).cpu().detach().numpy()
+
+    # run dim reduction with pca
+    pca = PCA(n_components=2)
+    X_reduced = pca.fit_transform(samples)
+
+    plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=labels.cpu().detach().numpy(), cmap='viridis')
+    plt.colorbar()
+    plt.title("PCA of Generated Samples")
+    plt.xlabel("PCA Component 1")
+    plt.ylabel("PCA Component 2")
+    pca_plot_path = os.path.join(output_dir, "pca_plot.png")
+    plt.savefig(pca_plot_path)
+    plt.close()
+
+    wandb.log({"PCA Plot": wandb.Image(pca_plot_path)})
+    print(f"PCA plot saved at {pca_plot_path}")
 
 
 def get_fid():
@@ -56,15 +71,18 @@ def eval_cgan(generator, discriminator, dataloader, params):
     output_dir = config.get("output_dir")
     num_samples = config.get("num_samples", 64)
 
-    # ensure directory exist
-    os.makedirs(output_dir, exist_ok=True)
-
     # load checkpoint if provided
     _ = load_checkpoint(checkpoint_path, generator, discriminator)
+
+    # generate run id
+    id = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    output_dir = os.path.join(output_dir, id)
+    os.makedirs(output_dir, exist_ok=True)
 
     # save some generated samples
     generated_samples, labels = save_generated_samples(generator, output_dir, num_samples)
 
     # pca
+    get_pca(generated_samples, labels, output_dir)
 
     # fid
